3bb5ac6221618f35ea17cb0376d2aaecde38795c
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-13 13:46:06 -0700

    added isDead

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -15,22 +15,33 @@ let jamieLannisterAttack = 35;
 //     console.log('These fighters are even matched')
 // }
 
-let jonSnowHealth = 100;
-let jonSnowDefense = 25;
+// let jonSnowHealth = 100;
+// let jonSnowDefense = 25;
 
-if (jonSnowHealth <= jamieLannisterAttack){
-    console.log('John is dead')
-} else {
-    jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
-    console.log(`Jom Snow health is down to ${jonSnowHealth}`)
+// if (jonSnowHealth <= jamieLannisterAttack){
+//     console.log('John is dead')
+// } else {
+//     jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+//     console.log(`Jom Snow health is down to ${jonSnowHealth}`)
+// }
+
+
+
+// if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
+//     console.log('Jon Snow is dead')
+// } else {
+//     jonSnowHealth -= jamieLannisterAttack - jonSnowDefense;
+//     console.log(`Jom Snow health is down to ${jonSnowHealth}`)
+// }
+
+for (let i = 1; i <= 3; i++){
+    isDead = Math.random() > .5
+    if (isDead === true){
+        console.log(`John has died after ${i} rounds`)
+        break
+    }
 }
 
-
-
-if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
-    console.log('Jon Snow is dead')
-} else {
-    jonSnowHealth -= jamieLannisterAttack - jonSnowDefense;
-    console.log(`Jom Snow health is down to ${jonSnowHealth}`)
+if (isDead === false) {
+    console.log('Its a mircale he made it')
 }
-
