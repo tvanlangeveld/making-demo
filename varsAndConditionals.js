c94e27c351bf4b76e1760a2ff84aa9e8f1273c27
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-13 13:53:16 -0700

    Final Commit

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -7,41 +7,52 @@
 let jonSnowAttack = 37;
 let jamieLannisterAttack = 35;
 
-// if(jonSnowAttack > jamieLannisterAttack){
-// console.log('john is better')
-// } else if(jamieLannisterAttack > jonSnowAttack) {
-//     console.log('jamie is beast and way better')
-// } else{
-//     console.log('These fighters are even matched')
-// }
+if(jonSnowAttack > jamieLannisterAttack){
+console.log('john is better')
 
-// let jonSnowHealth = 100;
-// let jonSnowDefense = 25;
+} else if(jamieLannisterAttack > jonSnowAttack) {
+    console.log('jamie is beast and way better')
+} else{
+    console.log('These fighters are even matched')
+}
 
-// if (jonSnowHealth <= jamieLannisterAttack){
-//     console.log('John is dead')
-// } else {
-//     jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
-//     console.log(`Jom Snow health is down to ${jonSnowHealth}`)
-// }
+let jonSnowHealth = 100;
+let jonSnowDefense = 25;
+
+if (jonSnowHealth <= jamieLannisterAttack){
+    console.log('John is dead')
+} else {
+    jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+    console.log(`Jom Snow health is down to ${jonSnowHealth}`)
+}
 
 
 
-// if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
-//     console.log('Jon Snow is dead')
-// } else {
-//     jonSnowHealth -= jamieLannisterAttack - jonSnowDefense;
-//     console.log(`Jom Snow health is down to ${jonSnowHealth}`)
-// }
+if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
+    console.log('Jon Snow is dead')
+} else {
+    jonSnowHealth -= jamieLannisterAttack - jonSnowDefense;
+    console.log(`Jom Snow health is down to ${jonSnowHealth}`)
+}
 
-for (let i = 1; i <= 3; i++){
+for (let i = 1; i <= 1; i++){
     isDead = Math.random() > .5
     if (isDead === true){
         console.log(`John has died after ${i} rounds`)
         break
     }
+    if (isDead === false) {
+        console.log('Its a mircale he made it')
+    
+        while (jonSnowHealth > 0){
+            console.log('Jamie punches Jon')
+            jonSnowHealth -= 5
+        }
+    console.log('John is dead')
+    }   
 }
 
-if (isDead === false) {
-    console.log('Its a mircale he made it')
-}
+
+
+
+
