1bfb35ff9a3b09daf04e11293a1472731bf2ec76
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-13 12:06:19 -0700

    adds jamie Health

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -7,12 +7,22 @@
 let jonSnowAttack = 37;
 let jamieLannisterAttack = 35;
 
-if(jonSnowAttack > jamieLannisterAttack){
-console.log('john is better')
-} else if(jamieLannisterAttack > jonSnowAttack) {
-    console.log('jamie is beast and way better')
-} else{
-    console.log('These fighters are even matched')
+// if(jonSnowAttack > jamieLannisterAttack){
+// console.log('john is better')
+// } else if(jamieLannisterAttack > jonSnowAttack) {
+//     console.log('jamie is beast and way better')
+// } else{
+//     console.log('These fighters are even matched')
+// }
+
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+if (jonSnowHealth <= jamieLannisterAttack){
+    console.log('John is dead')
+} else {
+    jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+    console.log(`Jom Snow health is down to ${jonSnowHealth}`)
 }
 
 
