9423a308fd71c3de5c2a78b1054921c2d31bb86c
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-14 10:38:40 -0700

    made good progress on pokemon lab

--- arrays.js
+++ arrays.js
@@ -1,4 +1,56 @@
 /*
-    In this file, we'll be continuing our story
+    In this file, we'll be making a new  story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = ['poke ball', 'poke ball', 'potion', 'beef jerky', 'running shoes']
+
+let pokemon = []
+
+backpack.push('oreos')
+backpack.push('oreos')
+
+console.log('after buying oreos, this is our back back')
+console.log(backpack)
+
+
+backpack.push('pokedex')
+pokemon.push('pikachu')
+
+console.log('after meeting professor oak we got it')
+console.log(backpack)
+console.log(pokemon)
+
+for (let i = 0; i < 4; i++){
+    pokemon.push('pidgey')
+}
+
+console.log('after the pidgeys, our pokemon are now')
+console.log(pokemon)
+
+backpack.splice(3,4)
+
+console.log(backpack)
+
+
+let newPokemon = 'charzard';
+
+pokemon.push(newPokemon)
+
+console.log(pokemon)
+
+
+console.log('here he is')
+
+let cutestPokemon = pokemon[0]
+
+console.log(cutestPokemon)
+
+
+let onlyPidgey = pokemon.slice(1,5)
+backpack.unshift('Jynx statue')
+
+
+console.log(onlyPidgey, backpack)
+
+
